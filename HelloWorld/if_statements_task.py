f4a8158bc62d16ea6be672e175d669fc1f3531e5
Mary Jackson <louesejackson@gmail.com>
2020-02-04 16:06:22 +0000

    Create if_statements_task.py

---
+++ HelloWorld/if_statements_task.py
@@ -0,0 +1,47 @@
+# Price of a house is One Million Dollars
+# If the buyer has good credit, 
+#     they need to put down 10%
+# Otherwise
+#     they need to put down 20%
+# Print the downpayment
+
+# This exercise is a bit discriminatory, but hey ho
+
+is_creditgood = False
+is_creditbad = False
+
+if is_creditgood:
+    print("Congratulations! Your deposit is 10%")
+    print("Your downpayment is 100k")
+
+elif is_creditbad:
+    print("Gosh, I'm sorry")
+    print("Your downpayment is 200k")
+
+else:
+    print("You will need to win the lottery to buy this house")
+
+print("Happy house hunting!")
+
+
+
+
+
+
+# is_hot = False
+# is_cold = False
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+
+# else:
+#     print("It's a lovely day")
+   
+
+# print("Enjoy your day")
+
+
+
