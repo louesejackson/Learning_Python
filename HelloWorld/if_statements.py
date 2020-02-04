a56d59b874ee73f52891c47eba1e2210a552b4a9
Mary Jackson <louesejackson@gmail.com>
2020-02-04 15:39:01 +0000

    Update if_statements.py

--- HelloWorld/if_statements.py
+++ HelloWorld/if_statements.py
@@ -1,8 +1,14 @@
-if it's hot
-    It's a hot day
-    Drink plenty of water
-otherwise if it's cold
-    It's a cold day
-    Wear warm clothes
-otherwise
-    It's a lovely day
+# if it's hot
+#     It's a hot day
+#     Drink plenty of water
+# otherwise if it's cold
+#     It's a cold day
+#     Wear warm clothes
+# otherwise
+    # It's a lovely day
+
+is_hot = True
+if is_hot:
+    print("It's a hot day")
+print("Enjoy your day")
+
